56d15579b2ab3b98b2d2ec4f48e1558bb426b653
Mackrine Awino <awinomackrine1@gmail.com>
2023-08-23 11:37:57 +0300

    Create calculator class for simple arithmetic

--- src/main/java/com/systechtraining/App.java
+++ src/main/java/com/systechtraining/App.java
@@ -1,28 +1,11 @@
 package com.systechtraining;
 
-import java.util.logging.Logger;
-
 public class App 
 {
-    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
-
     public static void main( String[] args )
     {
-        
-        int sum = addNumbers(5, 8);
-        int difference = subtractNumbers(5, 8);
-
-        LOGGER.info("The sum of the numbers is: "+ sum);
-        LOGGER.info("The Diffence of the numbers is: "+ difference);
-
-    }
-    public static int addNumbers(int a, int b)
-    {
-        return a+b;
-    } 
-    public static int subtractNumbers(int c, int d)
-    {
-        return c-d;
-
+        System.out.println( "Hello World!" );
+        System.out.println( "This is my first java program" );
+        System.out.println("The sum is: "+ (2+4));
     }
 }
---
+++ src/main/java/com/systechtraining/Calculator.java
@@ -0,0 +1,28 @@
+package com.systechtraining;
+
+
+import java.util.logging.Logger;
+
+public class Calculator {
+        private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+    public static void main( String[] args )
+    {
+        
+        int sum = addNumbers(5, 8);
+        int difference = subtractNumbers(5, 8);
+
+        LOGGER.info("The sum of the numbers is: "+ sum);
+        LOGGER.info("The Diffence of the numbers is: "+ difference);
+
+    }
+    public static int addNumbers(int a, int b)
+    {
+        return a+b;
+    } 
+    public static int subtractNumbers(int c, int d)
+    {
+        return c-d;
+
+    }
+}
