946169940f3c35d112b95e0bd488c000ee51f640
Mackrine Awino <awinomackrine1@gmail.com>
2023-08-24 07:25:20 +0300

    add step to run tests

--- src/test/java/com/systechtraining/CalculatorTest.java
+++ src/test/java/com/systechtraining/CalculatorTest.java
@@ -3,6 +3,9 @@ package com.systechtraining;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) 
 
 public class CalculatorTest {
     Calculator calculator = new Calculator(); 
