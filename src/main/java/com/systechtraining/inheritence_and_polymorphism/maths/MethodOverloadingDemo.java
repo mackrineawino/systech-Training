51d6a560414522831913f9cf7d8d831f4925c1b2
Mackrine Awino <awinomackrine1@gmail.com>
2023-09-18 11:02:36 +0300

    method overloading

---
+++ src/main/java/com/systechtraining/inheritence_and_polymorphism/maths/MethodOverloadingDemo.java
@@ -0,0 +1,14 @@
+package com.systechtraining.inheritence_and_polymorphism.maths;
+
+public class MethodOverloadingDemo {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.sum(15.23, 20.12);
+        calculator.sum(15, 20);
+        calculator.sum(15L, 20L);
+        calculator.sum(15L,20);
+        //! sum method is taking different forms depending on the number of arguments,
+        //!  data type, return type, access modifier, sequence of arguments
+        
+    }
+}
