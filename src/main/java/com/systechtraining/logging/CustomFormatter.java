62e5e87aace0e8a526c93de1e403635bc16d791e
Mackrine Awino <awinomackrine1@gmail.com>
2023-09-25 13:37:57 +0300

    logging using consoles and files

---
+++ src/main/java/com/systechtraining/logging/CustomFormatter.java
@@ -0,0 +1,20 @@
+package com.systechtraining.logging;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class CustomFormatter extends Formatter{
+     public String format(LogRecord record) {
+            DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String method = record.getSourceMethodName();
+            String level = record.getLevel().getName();
+            String message = record.getMessage();
+            Instant instant = record.getInstant();
+            LocalDateTime now = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+            return pattern.format(now) + " | " + method + " | " + level + " | " + message;
+        }
+}
