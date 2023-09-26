0a9b7c1919a0d91d227fc31c3eda43d1e7aba0a2
Mackrine Awino <awinomackrine1@gmail.com>
2023-09-26 16:35:56 +0300

    create custom exceptions

---
+++ src/main/java/com/systechtraining/pos/posWithDatabaseConnectivity/CustomException.java
@@ -0,0 +1,10 @@
+package com.systechtraining.pos.posWithDatabaseConnectivity;
+
+public class CustomException extends Exception{
+
+        public CustomException(String message){
+            super(message);
+        }
+        
+    
+}
