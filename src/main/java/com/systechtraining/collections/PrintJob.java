9983365c38388f6512cb2fba6f9563acf2eff254
Mackrine Awino <awinomackrine1@gmail.com>
2023-10-02 17:25:14 +0300

    working with collections

---
+++ src/main/java/com/systechtraining/collections/PrintJob.java
@@ -0,0 +1,27 @@
+package com.systechtraining.collections;
+
+public class PrintJob {
+
+
+    private String name;
+
+    public PrintJob(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PrintJob [name=" + name + "]";
+    }
+
+    
+    
+}
